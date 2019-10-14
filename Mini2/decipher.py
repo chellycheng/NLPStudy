1f5a3255971bc308308ab97b6a6de0ded9017e22
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2019-10-13 22:21:44 -0400

    Reader

---
+++ Mini2/decipher.py
@@ -0,0 +1,31 @@
+#import the useful library
+import os
+import sys
+#from nltk.tag.hmm import HiddenMarkovModelTrainer
+from pip._vendor.distlib.compat import raw_input
+
+
+class reader:
+    input_length = len(sys.argv)
+    boolean 
+    if()
+
+    s = input()
+    all_files = os.listdir(s+'/')
+    print(all_files)
+    input = [open(f, "r", encoding="utf-8") for f in all_files]
+    print(input)
+    #test_cipher, train_ciper, test_plain,train_plain = input[0],input[1]
+
+
+#class decoder1:
+
+    #trainer = HiddenMarkovModelTrainer()
+    #labled_sequence =
+    #estimator =
+    #trainer.train_supervised()
+class main:
+    reader()
+
+if __name__ == '__main__':
+    main()
